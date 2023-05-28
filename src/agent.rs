44459d18e5c0ed3ba55350c0586f036154a4bf37
Ray Hidayat <ray.hidayat@gmail.com>
2023-05-29 11:25:49 +1200

    Basic timing

--- src/agent.rs
+++ src/agent.rs
@@ -1,11 +1,15 @@
+use std::time::Instant;
 use super::inputs::*;
 use super::view::*;
 use super::evaluation;
 use super::plans::{self,*};
 
 pub fn act(view: &View, state: &State) -> Vec<Action> {
+    let start = Instant::now();
+
     let mut best = evaluate(Vec::new(), view, state);
     eprintln!("initial score: {}", best.score);
+    let mut num_evaluated = 1;
 
     for cell in 0..view.layout.cells.len() {
         if state.resources[cell] <= 0 { continue }
@@ -13,12 +17,16 @@ pub fn act(view: &View, state: &State) -> Vec<Action> {
         let candidate = evaluate(plan, view, state);
         eprintln!("candidate {} score: {}", cell, candidate.score);
 
+        num_evaluated += 1;
+
         if candidate.score > best.score {
             best = candidate;
         }
     }
 
     let actions = plans::enact_plan(ME, &best.plan, view, state);
+
+    eprintln!("evaluated {} plans in {:.0} ms", num_evaluated, start.elapsed().as_millis());
 
     actions
 }
