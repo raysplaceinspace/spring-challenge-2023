a1c04f933a2e97915e234a991e4f9204a947ca7f
Ray Hidayat <ray.hidayat@gmail.com>
2023-05-29 14:59:39 +1200

    Search nearest to mesh

--- src/plans.rs
+++ src/plans.rs
@@ -1,7 +1,10 @@
 use core::panic;
+use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::fmt::Display;
 
+use super::paths::PathMap;
 use super::inputs::*;
 use super::view::*;
 
@@ -15,25 +18,85 @@ impl Display for Milestone {
     }
 }
 
+struct HarvestMesh {
+    unharvested: HashMap<usize,i32>, // unharvested cell -> distance to closest beacon
+    priorities: VecDeque<usize>, // cells, highest priority first
+    beacons: HashSet<usize>,
+}
+impl HarvestMesh {
+    pub fn generate(plan: &[Milestone], view: &View, state: &State) -> Self {
+        let unharvested =
+            (0..view.layout.cells.len())
+            .filter(|i| state.resources[*i] > 0)
+            .map(|cell| (cell, i32::MAX))
+            .collect();
+        let priorities =
+            plan.iter()
+            .filter_map(|m| {
+                if state.resources[m.cell] > 0 {
+                    Some(m.cell)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        Self {
+            unharvested,
+            priorities,
+            beacons: HashSet::new(),
+        }
+    }
+
+    pub fn beacons(&self) -> impl Iterator<Item=usize> + '_ { self.beacons.iter().cloned() }
+    pub fn num_beacons(&self) -> usize { self.beacons.len() }
+
+    pub fn take_next(&mut self) -> Option<usize> {
+        if let Some(cell) = self.priorities.pop_front() {
+            // Take next priority cell
+            return Some(cell);
+        }
+
+        if let Some((cell, _)) = self.unharvested.iter().min_by_key(|(_,distance)| *distance) {
+            // Take closest unharvested cell
+            let cell = *cell;
+            self.unharvested.remove(&cell);
+            return Some(cell);
+        }
+
+        None
+    }
+
+    pub fn add_beacon(&mut self, beacon: usize, paths: &PathMap) {
+        if self.beacons.insert(beacon) {
+            for (cell, distance) in self.unharvested.iter_mut() {
+                let new_distance = paths.distance_between(*cell, beacon);
+                if new_distance < *distance {
+                    *distance = new_distance;
+                }
+            }
+        }
+    }
+}
+
 pub fn enact_plan(player: usize, plan: &[Milestone], view: &View, state: &State) -> Vec<Action> {
     let mut actions = Vec::new();
 
-    let my_base = view.layout.bases[player][0];
     let total_ants: i32 = state.num_ants[player].iter().cloned().sum();
 
-    let mut beacons = HashSet::new();
-    beacons.insert(my_base);
-
     let mut num_harvests = 0;
+    let mut mesh = HarvestMesh::generate(plan, view, state);
+    for &base in view.layout.bases[player].iter() {
+        mesh.add_beacon(base, &view.paths);
+    }
 
-    let sequence = calculate_harvest_sequence(player, plan, view, state);
-    for target in sequence {
-        let initial_distance = beacons.len() as i32;
+    while let Some(target) = mesh.take_next() {
+        let initial_distance = mesh.num_beacons() as i32;
         let initial_collection_rate = calculate_collection_rate(total_ants, initial_distance, num_harvests);
 
         if let Some((distance, source)) =
-            beacons.iter()
-            .map(|&source| (view.paths.distance_between(source, target),source))
+            mesh.beacons()
+            .map(|source| (view.paths.distance_between(source, target),source))
             .min() {
 
             let new_collection_rate = calculate_collection_rate(total_ants, initial_distance + distance, num_harvests + 1);
@@ -41,7 +104,7 @@ pub fn enact_plan(player: usize, plan: &[Milestone], view: &View, state: &State)
 
             if new_collection_rate > initial_collection_rate {
                 for cell in view.paths.calculate_path(source, target, &view.layout) {
-                    beacons.insert(cell);
+                    mesh.add_beacon(cell, &view.paths);
                 }
                 num_harvests += 1;
 
@@ -55,29 +118,11 @@ pub fn enact_plan(player: usize, plan: &[Milestone], view: &View, state: &State)
         }
     }
 
-    for beacon in beacons {
+    for beacon in mesh.beacons() {
         actions.push(Action::Beacon { index: beacon, strength: 1 });
     }
 
     actions
-}
-
-fn calculate_harvest_sequence(player: usize, plan: &[Milestone], view: &View, state: &State) -> Vec<usize> {
-    let prioritized: HashSet<usize> = plan.iter().map(|s| s.cell).collect();
-
-    let mut sequence: Vec<usize> = (0..view.layout.cells.len()).filter(|i| state.resources[*i] > 0 && !prioritized.contains(i)).collect();
-    let base = view.layout.bases[player][0];
-    sequence.sort_by_key(|&cell| view.paths.distance_between(base, cell));
-
-    sequence.splice(0..0, plan.iter().filter_map(|s| {
-        if state.resources[s.cell] > 0 {
-            Some(s.cell)
-        } else {
-            None
-        }
-    }));
-
-    sequence
 }
 
 fn calculate_collection_rate(total_ants: i32, total_distance: i32, num_harvests: i32) -> i32 {
