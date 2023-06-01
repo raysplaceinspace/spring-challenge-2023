1e5ebcf5c02d6f057d47e2a62f2897425a3e88f6
Ray Hidayat <ray.hidayat@gmail.com>
2023-06-01 19:10:38 +1200

    If no attack chains, skip extra calculation

--- src/harvesting.rs
+++ src/harvesting.rs
@@ -8,17 +8,18 @@ pub struct HarvestMap {
 }
 impl HarvestMap {
     pub fn generate(view: &View, num_ants: &AntsPerCellPerPlayer) -> Self {
-        let attack_chains_per_player = [
+        let max_flow_per_player = [
             calculate_max_flow_for_player(ME, view, num_ants),
             calculate_max_flow_for_player(ENEMY, view, num_ants),
         ];
 
-        let num_unhindered_ants = calculate_unhindered_ants(&attack_chains_per_player, num_ants);
-
-        let max_flow_per_player = [
-            calculate_max_flow_for_player(ME, view, &num_unhindered_ants),
-            calculate_max_flow_for_player(ENEMY, view, &num_unhindered_ants),
-        ];
+        let max_flow_per_player = match calculate_unhindered_ants(&max_flow_per_player, num_ants) {
+            Some(num_unhindered_ants) => [
+                calculate_max_flow_for_player(ME, view, &num_unhindered_ants),
+                calculate_max_flow_for_player(ENEMY, view, &num_unhindered_ants),
+            ],
+            None => max_flow_per_player, // unchanged
+        };
 
         Self { max_flow_per_player }
     }
@@ -66,20 +67,27 @@ fn calculate_flows_to_base(bases: impl Iterator<Item=usize>, layout: &Layout, nu
     flows.into_boxed_slice()
 }
 
-fn calculate_unhindered_ants(attack_chains_per_player: &[Box<[i32]>; NUM_PLAYERS], num_ants: &AntsPerCellPerPlayer) -> AntsPerCellPerPlayer {
+fn calculate_unhindered_ants(attack_chains_per_player: &[Box<[i32]>; NUM_PLAYERS], num_ants: &AntsPerCellPerPlayer) -> Option<AntsPerCellPerPlayer> {
     let mut unhindered = num_ants.clone();
     let num_cells = attack_chains_per_player[ME].len();
 
+    let mut changed = false;
     for cell in 0..num_cells {
         let my_attack_chain = attack_chains_per_player[ME][cell];
         let enemy_attack_chain = attack_chains_per_player[ENEMY][cell];
 
         if my_attack_chain > enemy_attack_chain {
             unhindered[ENEMY][cell] = 0;
+            changed = true;
         } else if enemy_attack_chain > my_attack_chain {
             unhindered[ME][cell] = 0;
+            changed = true;
         }
     }
 
-    unhindered
+    if changed {
+        Some(unhindered)
+    } else {
+        None
+    }
 }
