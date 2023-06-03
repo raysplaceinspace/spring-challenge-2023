b28523898f36e1eb0aebdc38d55336160d589ec3
Ray Hidayat <ray.hidayat@gmail.com>
2023-06-03 15:23:43 +1200

    Minimize movement from existing positions

--- src/planning.rs
+++ src/planning.rs
@@ -1,4 +1,4 @@
-use core::panic;
+use std::collections::BinaryHeap;
 
 use super::fnv::FnvHashSet;
 use std::fmt::Display;
@@ -42,30 +42,31 @@ pub fn enact_plan(player: usize, plan: &[Milestone], view: &View, state: &State)
         let target = milestone.cell;
         if state.resources[target] <= 0 { continue } // Nothing to harvest here
 
-        if let Some((distance, source)) = beacons.iter().map(|&source| (view.paths.distance_between(source, target),source)).min() {
-            let content = view.layout.cells[target].content;
-            let new_counts = counts.clone().add(content);
+        let closest_beacon = beacons.iter().min_by_key(|&&beacon| {
+            view.paths.distance_between(beacon, target)
+        }).cloned().expect("no beacons");
 
-            let initial_spread = beacons.len() as i32;
-            let new_collection_rate = evaluator.calculate_harvest_rate(&new_counts, initial_spread + distance);
-            // eprintln!("considered harvesting <{}> (distance {}): {} -> {}", target, distance, initial_collection_rate, new_collection_rate);
+        let path = find_closest_harvest_chain(closest_beacon, target, view, &nearby);
 
-            let initial_collection_rate = evaluator.calculate_harvest_rate(&counts, initial_spread);
-            if new_collection_rate > initial_collection_rate {
-                for cell in nearby.calculate_path(source, target, &view.layout, &view.paths) {
-                    beacons.insert(cell);
-                }
-                targets.push(target);
+        let content = view.layout.cells[target].content;
+        let new_counts = counts.clone().add(content);
 
-                counts = new_counts;
+        let initial_spread = beacons.len() as i32;
+        let initial_collection_rate = evaluator.calculate_harvest_rate(&counts, initial_spread);
 
-            } else {
-                // Best harvest not worth it, so none others will be either
-                break;
+        let new_spread = initial_spread + path.len().saturating_sub(1) as i32; // -1 because the first cell is already a beacon
+        let new_collection_rate = evaluator.calculate_harvest_rate(&new_counts, new_spread);
+        if new_collection_rate > initial_collection_rate {
+            for cell in path {
+                beacons.insert(cell);
             }
+            targets.push(target);
+
+            counts = new_counts;
 
         } else {
-            panic!("no sources available for harvest");
+            // Best harvest not worth it, so none others will be either
+            break;
         }
     }
 
@@ -96,4 +97,47 @@ impl Display for Commands {
         }
         Ok(())
     }
+}
+
+fn find_closest_harvest_chain(source: usize, target: usize, view: &View, nearby: &NearbyPathMap) -> Vec<usize> {
+    if source == target { return vec![source] }
+
+    let num_cells = view.layout.cells.len();
+
+    let mut cost_map = Vec::new();
+    cost_map.resize(num_cells, i32::MAX);
+
+    let mut queue = BinaryHeap::new();
+    queue.push((0, target));
+    cost_map[target] = 0;
+
+    while let Some((priority, cell)) = queue.pop() {
+        if cell == source { break }
+
+        let cost = -priority;
+        for &n in view.layout.cells[cell].neighbors.iter() {
+            let length_cost = 1; // prefer shorter paths so we don't spread our ants too thinly
+            let step_cost = (nearby.distance_to(n) - 1).max(0); // prefer paths closer to our ants. -1 because our ants can move 1 cell per tick for free.
+            let new_cost = cost + length_cost + step_cost;
+            if new_cost < cost_map[n] {
+                cost_map[n] = new_cost;
+                queue.push((-new_cost, n));
+            }
+        }
+    }
+
+    let mut path = vec![source];
+    loop {
+        let end = path.last().expect("unexpected empty path").clone();
+        if end == target { break }
+
+        let next =
+            view.layout.cells[end].neighbors.iter()
+            .min_by_key(|&&n| cost_map[n])
+            .expect("no neighbor found in cost map")
+            .clone();
+        path.push(next);
+    }
+
+    path
 }
